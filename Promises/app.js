60f6db98d825629089f9f908a821cdd2b26386bf
urosm93 <urkez93@icloud.com>
2018-05-29 20:04:50 +0200

    Promise cats task done

--- Promises/app.js
+++ Promises/app.js
@@ -1,44 +1,50 @@
 
-const random1 = new Promise ((resolve,reject) =>{
+const random1 = new Promise((resolve, reject) => {
     $.ajax("http://thecatapi.com/api/images/get?format=xml&results_per_page=1")
-        
-    .done((response) => {
-        const imgUrl = $(response).find("url").text();
-        resolve(imgUrl);
-    });
-});
 
-const random2 = new Promise ((resolve,reject) =>{
-     $.ajax({
-         url: `http://thecatapi.com/api/images/get?format=xml&results_per_page=1`,
-         method: "GET"
-    })
-    .done((response) => {
-        const imgUrl = $(response).find("url").text();
-        resolve(imgUrl);
-    });
- });
+        .done(res => resolve(res))
+        .fail(() => {
+            reject("Network problem!")
+        })
+})
 
-
- const random3 = new Promise ((resolve,reject) =>{
+const random2 = new Promise((resolve, reject) => {
     $.ajax({
         url: `http://thecatapi.com/api/images/get?format=xml&results_per_page=1`,
         method: "GET"
-   })
-   .done((response) => {
-       const imgUrl = $(response).find("url").text();
-       resolve(imgUrl);
-   });
-});
+    })
+        .done(res => resolve(res))
 
+        .fail(() => {
+            reject("Network problem!")
+        })
+})
+
+
+const random3 = new Promise((resolve, reject) => {
+    $.ajax({
+        url: `http://thecatapi.com/api/images/get?format=xml&results_per_page=1`,
+        method: "GET"
+    })
+        .done(res => resolve(res))
+
+        .fail(() => {
+            reject("Network problem!")
+        })
+})
+const body = $("body");
 Promise.all([random1, random2, random3])
-.then((data)=> {
-    const content = $(".images");
-    data.forEach(element =>{
-        const img = $(`<img class="imgs" src=${element} >`);
+    .then((res) => {
+        const content = $(".images");
+        res.forEach(element => {
+            const url = $(element).find("url").text();
+            const img = $(`<img class="imgs" src=${url} >`);
             content.append(img);
 
+        })
+            
+          
+
     })
-
-
-})
+    .catch((msg) => console.log(msg))
+    .finally(() => body.prepend("<h1>Cats</h1>"));
